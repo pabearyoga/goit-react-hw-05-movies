989cac78a4f2a20e714c50bf1f959f9a47e0a551
Pabearyoga <pabearyoga@gmail.com>
2022-10-01 13:57:47 +0300

    fix prevState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,13 +15,13 @@ const App = () => {
   const  addFeedback = event => {
     switch (event) {
       case 'good':
-            setGood(good + 1)
+            setGood((prevState) => (prevState + 1))
         break;
       case 'neutral':
-            setNeutral(neutral + 1)
+            setNeutral((prevState) => (prevState + 1))
         break;
       case 'bad':
-            setBad(bad + 1)
+            setBad((prevState) => (prevState + 1))
         break;
       default:
         break;
